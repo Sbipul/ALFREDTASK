1cd2f76af3c94969da49df04aee0bbb46af4c3b9
Bipul Chandro Roy <86865606+Sbipul@users.noreply.github.com>
2022-02-13 16:01:54 +0600

    Update BlockA.js

--- src/Pages/Home/Banner/BlockA/BlockA.js
+++ src/Pages/Home/Banner/BlockA/BlockA.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const BlockA = () => {
     return (
         <div className="custom_bg griD text-white my-6 p-3" style={{overflow:'hidden',borderRadius:'5px'}}>
-            <Link className="d-block my-2 text-decoration-none text-light" to="/home"><h5>Homeeeeeeeeeeeeeeeeeeeeeeeeeeeee</h5></Link> 
+            <Link className="d-block my-2 text-decoration-none text-light" to="/home"><h5>Homeeeeeeeeeeeeeeeeeeeeeeeeeeee</h5></Link> 
             <Link className="d-block my-2 text-decoration-none text-light" to="/dash"><h5>Search Place</h5></Link> 
             <Link className="d-block my-2 text-decoration-none text-light" to="/places"><h5>Your Places</h5></Link> 
         </div>
